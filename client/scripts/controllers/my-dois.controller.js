fc542bb5ef1513a62befc1189f7e86565489db67
Jody Salt <jody.salt@stfc.ac.uk>
2016-09-15 16:02:59 +0100

    implemented buildQuery function

--- client/scripts/controllers/my-dois.controller.js
+++ client/scripts/controllers/my-dois.controller.js
@@ -8,6 +8,35 @@
     app.controller('MyDoisController', function(tc){
        
         var page = 1;
+        var pageSize = 10;
+
+        function buildQuery(){
+            return [
+                "select dataCollection from",
+                "DataCollection dataCollection, ",
+                //all datacollections with a datafile in an investigation user's investigation
+                "dataCollection.dataCollectionDatafiles as dataCollectionDatafile, ",
+                "dataCollectionDatafile.datafile as datafile, ",
+                "datafile.dataset as datasetViaDatafile, ",
+                "datasetViaDatafile.investigation as investigationViaDatafile, ",
+                "investigationViaDatafile.investigationUsers as investigationUserViaDatafile, ",
+                "investigationUserViaDatafile.user as userViaDatafile, ",
+                //all datacollections with a dataset in an investigation user's investigation
+                "dataCollection.dataCollectionDatasets as dataCollectionDataset, ",
+                "dataCollectionDataset.dataset as dataset, ",
+                "dataset.investigation as investigationViaDataset, ",
+                "investigationViaDataset.investigationUsers as investigationUserViaDataset, ",
+                "investigationUserViaDataset.user as userViaDataset ",
+                //where a doi has been issued
+                //and the current user is the investigation user
+                "where ",
+                "dataCollection.doi != null ",
+                "and ",
+                "(userViaDatafile.name = :user or userViaDataset.name = :user) ",
+
+                "limit ?, ?", (page - 1) * pageSize, pageSize
+            ]
+        }
 
         function getPage(){
 
@@ -17,6 +46,3 @@
 
 })();
 
-//show any DataCollections with a DOI, with datafiles or datacollections inside of investigations you belong to
-
-//"select dataCollection from DataCollection dataCollection, dataCollection.dataCollectionDatafiles as dataCollectionDatafile, dataCollectionDatafile.datafile as datafile, datafile.dataset as datasetViaDatafile, datasetViaDatafile.investigation as investigationViaDatafile, investigationViaDatafile.investigationUsers as investigationUserViaDatafile, investigationUserViaDatafile.user as userViaDatafile, dataCollection.dataCollectionDatasets as dataCollectionDataset, dataCollectionDataset.dataset as dataset, dataset.investigation as investigationViaDataset, investigationViaDataset.investigationUsers as investigationUserViaDataset, investigationUserViaDataset.user as userViaDataset where dataCollection.doi != null and (userViaDatafile.name = :user or userViaDataset.name = :user)"
